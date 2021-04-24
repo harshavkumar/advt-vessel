69596bcd951c38b92049a4053bf8cb35ca85d58a
Harshav Kumar <harshav.kumar@rmgx.in>
2021-04-25 01:39:44 +0530

    advt placment paper model

--- models/model.py
+++ models/model.py
@@ -16,57 +16,26 @@ class MODEL(BASE):
 		model_name = 'morph'
 		super(MODEL, self).__init__(config, model_name)
 
-	def compose_model(self,pretrained_weights = None):
-		inputs = keras.layers.Input(shape=tuple(self.config.MODEL['MODEL_PARAMS']['INPUT_SHAPE']))
-		conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-		conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-		pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-		conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-		conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-		pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-		conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-		conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-		pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-		conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-		conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-		drop4 = Dropout(0.5)(conv4)
-		pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+	def compose_model(self,pretrained_weights = None,input_size = (200,200,3),weight_decay=0):
+		inp = keras.layers.Input(shape=tuple(self.config.MODEL['MODEL_PARAMS']['INPUT_SHAPE']))
 
-		conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-		conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-		drop5 = Dropout(0.5)(conv5)
+		# Encoder
+		x = keras.layers.Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu')(inp)
+		x = keras.layers.MaxPool2D(pool_size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu')(x)
+		x = keras.layers.MaxPool2D(pool_size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu')(x)
+		x = keras.layers.MaxPool2D(pool_size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu')(x)
+		# Decoder
+		x = keras.layers.UpSampling2D(size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=8, kernel_size=(3, 3), padding='same', activation='relu')(x)
+		x = keras.layers.UpSampling2D(size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=16, kernel_size=(3, 3), padding='same', activation='relu')(x)
+		x = keras.layers.UpSampling2D(size=(2, 2))(x)
+		x = keras.layers.Conv2D(filters=1, kernel_size=(3, 3), padding='same', activation='sigmoid')(x)
 
-		up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-		merge6 = concatenate([drop4,up6], axis = 3)
-		conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-		conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-		up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-		merge7 = concatenate([conv3,up7], axis = 3)
-		conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-		conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-		up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-		merge8 = concatenate([conv2,up8], axis = 3)
-		conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-		conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-		up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-		merge9 = concatenate([conv1,up9], axis = 3)
-		conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-		conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-		conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-		conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-
-		model = Model(input = inputs, output = conv10)
-
-		model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
-		
-		#model.summary()
-
-		if(pretrained_weights):
-			model.load_weights(pretrained_weights)
-		
-
+		model = keras.models.Model(inputs=inp, outputs=x)
+		model.compile(loss='binary_crossentropy', optimizer=keras.optimizers.Adam())
 
 		return model
